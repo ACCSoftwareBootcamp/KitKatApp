968b303973803f05dd6053a39034255ae8263fdd
jaimmikabouverette <bjaimmika@gmail.com>
2022-01-20 19:10:06 -0600

    updating local files

--- kitkatapp/src/App.js
+++ kitkatapp/src/App.js
@@ -2,16 +2,13 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NavBar from './Components/Navbar';
 import Home from './Components/Home';
-import Login from './Components/Login';
-import Register from './Components/Register';
 
 function App() {
   return (
     <div className="App">
      <NavBar /> 
-      {/* <Home /> */}
-      {/* <Login /> */}
-    <Register />
+      <Home />
+
     </div>
   );
 }
